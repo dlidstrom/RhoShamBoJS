9578c351b26248e94efab2ddd5c65d1b1c75b916
Daniel Lidström <dlidstrom@gmail.com>
2014-12-23 14:57:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,18 @@
     'use strict';
     
     var h = [];
-    for (var i = 0; i < 10; i++) {
-        h.push(1);
-        h.push(2);
+    for (var i = 0; i < 6; i++) {
+        h.push(i);
     }
     
     var sums = {};
     var hLength = h.length;
-    for (var i = 0; i < hLength; i++) {
-        for (var j = i + 1; j <= hLength; j++) {
+    for (var i = 0; i < hLength - 1; i++) {
+        for (var j = i + 1; j < hLength; j++) {
             var key = h.slice(i, j).join();
             sums[key] = sums[key] || { '0': 0, '1': 0, '2': 0 };
-            sums[key][h[j]]++;
+            var next = h[j];
+            sums[key][next]++;
         }
     }
 })();
