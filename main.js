c32b88757d78b2a405f94a178e5822a023512b29
Daniel Lidström <dlidstrom@gmail.com>
2014-12-23 14:48:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
-(function(){
-  'use strict';
-  
-  var h = [];
-  var constKey = '';
-  for (var i = 0; i < 10; i++) {
-    h.push(1);
-    h.push(2);
-    constKey += '  ';
-  }
-  
-  var sums = {};
-  var hLength = h.length;
-  for (var i = 0; i < hLength - 1; i++) {
-    for (var j = i + 1; j < hLength; j++) {
-      var key = constKey.substring(0, j - i);
-      for (var c = 0, n = j - i; c < n; c++) {
-        key[c] = h[c];
-      }
-      
-      sums[key] = sums[key] || {'0': 0, '1': 0, '2': 0};
-      sums[key][h[j]]++;
+(function () {
+    'use strict';
+    
+    var h = [];
+    for (var i = 0; i < 10; i++) {
+        h.push(1);
+        h.push(2);
     }
-  }
+    
+    var sums = {};
+    var hLength = h.length;
+    for (var i = 0; i < hLength - 1; i++) {
+        for (var j = i + 1; j < hLength; j++) {
+            var key = h.slice(i, j).join();
+            sums[key] = sums[key] || { '0': 0, '1': 0, '2': 0 };
+            sums[key][h[j]]++;
+        }
+    }
 })();
